e13537cb60196c1f0c21d6a29662119934f39e16
JackUait <jackuait@bk.ru>
2020-04-05 07:54:29 +0300

    main.js deleted

--- js/main.js
+++
@@ -1,6 +0,0 @@
-let nav__icon = document.querySelector('.nav__icon');
-let nav__list = document.querySelector('.nav__list');
-
-nav__icon.addEventListener('click', function(event) {
-    nav__list.classList.toggle('nav__list_is-opened');
-});
