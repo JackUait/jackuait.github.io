efa23de9a10fc9b44cd3cf7e1d8006a47ec4b16d
JackUait <jackuait@bk.ru>
2020-04-05 07:58:05 +0300

    Adde a new website

---
+++ pages/quotes/build/precache-manifest.4722d36e9e0e6a602e34c6e1c27e103d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dad1d5d975f036e409fc4ea5bb31709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e019bc68950fa9464ed7",
+    "url": "/static/css/main.1b9414c1.chunk.css"
+  },
+  {
+    "revision": "bbb0867f9da3cc8bc20d",
+    "url": "/static/js/2.b972dcac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b972dcac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e019bc68950fa9464ed7",
+    "url": "/static/js/main.5a4c1410.chunk.js"
+  },
+  {
+    "revision": "2b4fa0eb33bca3aa5bf0",
+    "url": "/static/js/runtime-main.46977ced.js"
+  }
+]);
