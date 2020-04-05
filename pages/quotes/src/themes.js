9ea5296ffb1c3ad5de29065b5b7ee408884e55fb
JackUait <jackuait@bk.ru>
2020-04-05 07:14:41 +0300

    Changed the primary website and added a new quotes website

---
+++ pages/quotes/src/themes.js
@@ -0,0 +1,16 @@
+const themes = [
+	{
+		value: "greatQuotes",
+		text: "Цитаты великих людей",
+	},
+	{
+		value: "favorite",
+		text: "Любимые",
+	},
+	{
+		value: "numbers",
+		text: "Числа",
+	},
+];
+
+export default themes;
