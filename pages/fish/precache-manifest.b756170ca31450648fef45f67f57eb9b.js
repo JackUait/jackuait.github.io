e97830a1dd15defe30cbfbc167b362f058b73dab
JackUait <jackuait@bk.ru>
2020-05-18 19:59:44 +0300

    meme

---
+++ pages/fish/precache-manifest.b756170ca31450648fef45f67f57eb9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbcee23c0d758722df1e8b2403d20d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d698e896df0e50ff6586",
+    "url": "/static/css/main.afec933d.chunk.css"
+  },
+  {
+    "revision": "ef3d8086010c67ba23a4",
+    "url": "/static/js/2.4154ed23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4154ed23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d698e896df0e50ff6586",
+    "url": "/static/js/main.6c9123fc.chunk.js"
+  },
+  {
+    "revision": "044156c01fb44fb7a9a0",
+    "url": "/static/js/runtime-main.b921a358.js"
+  }
+]);
